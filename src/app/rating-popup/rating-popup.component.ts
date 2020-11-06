7d9ebdeb0d02f4b6d912ee319df87b17ce4426f1
Zeyad Ahmad Ramadan <Zeyad.A.Ramadan@Gmail.com>
2020-11-06 20:14:57 +0200

    Adjusting stars position dynamically

--- src/app/rating-popup/rating-popup.component.ts
+++ src/app/rating-popup/rating-popup.component.ts
@@ -1,17 +1,14 @@
-import { Inject, OnInit } from '@angular/core';
+import { HostListener, Inject, OnDestroy, OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import {
-  MAT_DIALOG_DATA,
-  MatDialog,
-  MatDialogRef,
-} from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-rating-popup',
   templateUrl: './rating-popup.component.html',
   styleUrls: ['./rating-popup.component.scss'],
 })
-export class RatingPopupComponent implements OnInit {
+export class RatingPopupComponent implements OnInit, OnDestroy {
   // tslint:disable-next-line: no-inferrable-types
   ratingVal: number = 0;
   hoveredVal: number = null;
@@ -22,17 +19,51 @@ export class RatingPopupComponent implements OnInit {
     .fill(null)
     .map((num, i) => i + 1);
 
+  messagesRow: HTMLDivElement;
+  ratingRows: HTMLCollectionOf<HTMLDivElement>;
+
+  subscriptionsArr: Subscription[] = [];
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: { msg: string },
-    private dialogRef: MatDialogRef<RatingPopupComponent>
+    private matDialogRef: MatDialogRef<RatingPopupComponent>
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscriptionsArr.push(
+      this.matDialogRef.afterOpened().subscribe(() => {
+        this.messagesRow = document.getElementById(
+          'rating-message-row'
+        ) as HTMLDivElement;
+        this.ratingRows = document.getElementsByClassName(
+          'rating-stars-row'
+        ) as HTMLCollectionOf<HTMLDivElement>;
+
+        this.adjustElementsMargins();
+      })
+    );
+  }
+
+  @HostListener('window:resize')
+  adjustElementsMargins(): void {
+    this.messagesRow.style.marginBottom = `calc(${this.ratingRows[0].clientHeight}px + 1rem)`;
+
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.ratingRows.length; i++) {
+      const ratingRow = this.ratingRows[i];
+      ratingRow.style.top = `calc(${this.messagesRow.clientHeight}px + 0.5rem)`;
+    }
+  }
 
   close(type?: string): void {
     if (type === 'confirm') {
-      if (this.ratingVal) this.dialogRef.close(this.ratingVal);
+      if (this.ratingVal) this.matDialogRef.close(this.ratingVal);
       else this.showErrMsg = true;
-    } else if (type === 'cancel') this.dialogRef.close();
+    } else if (type === 'cancel') this.matDialogRef.close();
+  }
+
+  ngOnDestroy(): void {
+    for (const subscription of this.subscriptionsArr)
+      subscription.unsubscribe();
   }
 }
